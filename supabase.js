b1bd623f727de66c61f93dd3e621e1beabc6f066
wedscoop <contact.wedscoop@gmail.com>
2025-12-11 20:19:51 +0530

    Fixed app unavailable issue. Probably due to key rotation

--- supabase.js
+++ supabase.js
@@ -1,6 +1,7 @@
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
+// supabase.js
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2.31.0/+esm';
 
-const supabaseUrl = 'https://qkhokdmtloufbbcajmst.supabase.co';  // <-- Replace this
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFraG9rZG10bG91ZmJiY2FqbXN0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE0ODA4NjksImV4cCI6MjA2NzA1Njg2OX0.86U7qFrZ59h--B0yyENgrLyF3mtoZ8FdFrQU87tTTiY';                    // <-- Replace this
+const SUPABASE_URL = 'https://qkhokdmtloufbbcajmst.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFraG9rZG10bG91ZmJiY2FqbXN0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE0ODA4NjksImV4cCI6MjA2NzA1Njg2OX0.86U7qFrZ59h--B0yyENgrLyF3mtoZ8FdFrQU87tTTiY';
 
-export const supabaseClient = createClient(supabaseUrl, supabaseKey);
+export const supabaseClient = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
